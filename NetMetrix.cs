11e1e289387f1a6827c3c744529641b0970eed45
Gonzalo Casas <gonzalo.casas@local.ch>
2014-09-28 03:38:23 +0200

    Add configuration file

--- NetMetrix.cs
+++ NetMetrix.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Xml.Linq;
 using Microsoft.Phone.Controls;
 
 namespace NetMetrixSdk
@@ -15,11 +16,30 @@ namespace NetMetrixSdk
         static NetMetrix()
         {
             Host = Debugger.IsAttached ? "wemfbox-test.ch" : "wemfbox.ch";
+
+            var root = XDocument.Load(@"Properties\NetMetrix.xml").Root;
+            if (root == null) throw new InvalidOperationException("No NetMetrix.xml configuration file found");
+
+            var offer = root.Element("OfferID");
+            if (offer == null || string.IsNullOrWhiteSpace(offer.Value)) throw new InvalidOperationException("OfferID missing in NetMetrix.xml");
+            OfferId = offer.Value;
+
+            var app = root.Element("AppID");
+            if (app == null || string.IsNullOrWhiteSpace(app.Value)) throw new InvalidOperationException("AppID missing in NetMetrix.xml");
+            AppId = app.Value;
         }
 
         public static Tracker Tracker
         {
             get { return tracker; }
+        }
+
+        public static void EnableNavigationTracker(PhoneApplicationFrame appFrame)
+        {
+            appFrame.Navigated += (sender, args) =>
+            {
+                Tracker.Track();
+            };
         }
     }
 }
